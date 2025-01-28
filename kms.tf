7f42bb9018939ab45d54a2d8744ef7750d651d13
Huseyin Unal <23301714+husunal@users.noreply.github.com>
2025-01-28 16:10:51 +0000

    feat: add custom role for Vault KMS access and update IAM permissions

--- kms.tf
+++ kms.tf
@@ -28,20 +28,17 @@ resource "google_project_iam_member" "vault_iam_kms" {
   member  = google_service_account.vault_sa.member
 }
 
+resource "google_project_iam_custom_role" "vault_kms_custom_role" {
+  role_id     = "vaultKmsRole"
+  title       = "Vault KMS CryptoKey Encrypter/Decrypter and Viewer Role"
+  description = "Custom role for Vault to access Cloud KMS Key"
+  permissions = var.vault_kms_custom_role
+}
+
 resource "google_kms_crypto_key_iam_member" "vault_key" {
   count = var.gcp_kms_crypto_key == null ? 0 : 1
 
   crypto_key_id = data.google_kms_crypto_key.vault_key[0].id
-  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  role          = google_project_iam_custom_role.vault_kms_custom_role.name
   member        = google_service_account.vault_sa.member
-}
-resource "google_kms_key_ring_iam_binding" "vault_iam_kms_binding" {
-  count       = var.gcp_kms_key_ring == null ? 0 : 1
-  key_ring_id = data.google_kms_key_ring.vault[0].id
-  # key_ring_id = "${var.gcloud-project}/${var.keyring_location}/${var.key_ring}"
-  role = "roles/editor"
-
-  members = [
-    google_service_account.vault_sa.member,
-  ]
 }
